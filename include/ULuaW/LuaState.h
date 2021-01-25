0466a9c488545adb4047c5acd6d9939f1a456ac3
Ubpa <ustczt@mail.ustc.edu.cn>
2021-01-25 15:54:22 +0800

    add test stack

--- include/ULuaW/LuaState.h
+++ include/ULuaW/LuaState.h
@@ -23,6 +23,15 @@ namespace Ubpa {
         LuaState(std::shared_ptr<lua_State> L, std::shared_ptr<lua_State> main);
         LuaState(std::shared_ptr<lua_State> L);
 
+        std::shared_ptr<lua_State> GetState()        { return L;          }
+        std::shared_ptr<lua_State> GetMainState()    { return main;       }
+        lua_State*                 GetRawState()     { return L.get();    }
+        lua_State*                 GetRawMainState() { return main.get(); }
+
+        //
+        // Lua raw API
+        ////////////////
+
         int absindex(int idx);
         void arith(int op);
         lua_CFunction atpanic(lua_CFunction panicf);
