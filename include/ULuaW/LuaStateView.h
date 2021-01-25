ae3e3309cc825ffe173720f10d1597adc365f01d
Ubpa <ustczt@mail.ustc.edu.cn>
2021-01-25 17:36:52 +0800

    fix a bug

--- include/ULuaW/LuaStateView.h
+++ include/ULuaW/LuaStateView.h
@@ -6,9 +6,11 @@ namespace Ubpa {
     class LuaStateView {
     public:
 
+        LuaStateView() : L{ nullptr } {}
         LuaStateView(lua_State* L) : L{ L } {}
 
         lua_State* GetState() { return L; }
+        bool Valid() const { return L != nullptr; }
         operator lua_State* () { return L; }
 
         int absindex(int idx);
